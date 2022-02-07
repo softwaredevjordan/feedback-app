8b7f64541ac75ec3de57e1527a5859335b5c58f6
Jordan H <webdevjordan@protonmail.com>
2022-02-07 10:48:53 -0600

    missed an import

--- src/component/FeedbackItem.jsx
+++ src/component/FeedbackItem.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useState } from 'react'
 import Card from './shared/Card.jsx';
 import PropTypes from 'prop-types';
 import {FaTimes, FaEdit} from 'react-icons/fa'
